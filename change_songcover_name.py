f587c5c28c8b87e3fd1e773145813d0f908628bb
Dinorex <1819539569@qq.com>
2024-10-07 00:26:31 +0800

    Arcaea,Paradigm联动

--- change_songcover_name.py
+++ change_songcover_name.py
@@ -9,9 +9,14 @@ files = os.listdir(folder_path)
 # 循环处理每个文件
 for filename in files:
     # 检查文件名中是否包含 "-"
-    if '-' in filename:
-        # 将 "-" 替换为 " "
-        new_filename = filename.replace('-', ' ')
+    if ('-' in filename) or ('200' in filename):
+        if '-' in filename:
+            # 将 "-" 替换为 " "
+            new_filename = filename.replace('-', ' ')
+        
+        if '200' in filename:
+            # 将 "200" 替换为 "100"
+            new_filename = new_filename.replace('200', '100')
         
         # 构建新的文件路径
         old_path = os.path.join(folder_path, filename)
